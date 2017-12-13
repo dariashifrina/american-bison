5b3ec7de668f94c233c270b3b8cace5e12283250
dariashifrina <dshifrina@stuy.edu>
2017-12-13 18:08:04 -0500

    done

--- find.js
+++ find.js
@@ -1,14 +1,25 @@
+/*
+American Bison - Daria Shifrina & Tiffany Moi
+SoftDev1 pd7
+K #17: Moo? 
+2017-12-13
+*/
+
 var box = document.getElementById("box");
 var boxHeight = box.offsetHeight;
 var boxWidth = box.offsetWidth;
 
 //hardcode target as center
 //randomize later
-//var targetX = boxWidth / 2;
-//var targetY = boxHeight / 2;
+var targetX = boxWidth / 2;
+var targetY = boxHeight / 2;
+var max_dist = 1;
+var current_dist = 1;
 
-var targetX = Math.random() * boxWidth;
-var targetY = Math.random() * boxHeight
+var setIt = function(){
+    targetX = Math.random() * boxWidth;
+    targetY = Math.random() * boxHeight;
+}
 
 console.log( "box height: " + boxHeight );
 console.log( "box width: " + boxWidth );
@@ -18,17 +29,23 @@ var distance = function (x0, y0, x1, y1) {
     return Math.sqrt(Math.pow((x0-x1),2) + Math.pow((y0 - y1),2));
 };
 
+var foundIt = function(e) {
+    var dilution = current_dist/max_dist;
+    console.log(dilution);
+    if (dilution <= 0.02){
+	alert("yayyyyy you won! Click to play again!");
+	setIt();
+    }
+    
+    
+};
 
 var findIt = function(e) {
-    var max_dist = findMax(test_dist()); 
-    var current_dist = distance(e.offsetX, e.offsetY, targetX, targetY);
+    max_dist = findMax(test_dist()); 
+    current_dist = distance(e.offsetX, e.offsetY, targetX, targetY);
     var dilution = current_dist/max_dist;
     console.log(dilution);
     box.style.backgroundColor = "rgba(255,0,0," + dilution + ")";
-    if (Math.round(dilution) == 0){
-	alert("yayyyyy you won!");
-    }
-    
 };
 
 //test out all corners of the box
@@ -50,4 +67,6 @@ var findMax = function(arr){
     return max;
 };
 
+setIt();
 box.addEventListener("mousemove", findIt);
+box.addEventListener("click", foundIt);
